0ecc162e8fb3a27852c2edc9ed97664b38f04b06
Petteri <26197131+PetteriM1@users.noreply.github.com>
2023-05-14 13:59:20 +0300

    Separate edit command

--- src/main/java/me/petterim1/seeinv/Main.java
+++ src/main/java/me/petterim1/seeinv/Main.java
@@ -1,8 +1,14 @@
 package me.petterim1.seeinv;
 
 import cn.nukkit.Player;
+import cn.nukkit.block.BlockEnderChest;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
+import cn.nukkit.event.inventory.InventoryCloseEvent;
+import cn.nukkit.event.inventory.InventoryOpenEvent;
+import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.ListTag;
@@ -11,41 +17,46 @@ import com.nukkitx.fakeinventories.inventory.ChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.DoubleChestFakeInventory;
 import com.nukkitx.fakeinventories.inventory.FakeSlotChangeEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends PluginBase {
 
+    private final List<String> ecOpen = new ArrayList<>();
+    private final List<String> invOpen = new ArrayList<>();
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§cYou can run this command only as a player");
+            return true;
+        }
+
+        if (args.length == 0) {
+            return false;
+        }
+
+        Player target = getServer().getPlayer(args[0]);
+        CompoundTag offlineData = null;
+
+        if (target == null) {
+            offlineData = getServer().getOfflinePlayerData(args[0], false);
+            if (offlineData == null) {
+                sender.sendMessage("§cUnknown player: " + args[0]);
+                return true;
+            }
+        }
+
+        ChestFakeInventory inv;
+
         if (cmd.getName().equals("seeinv")) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage("§cYou can run this command only as a player");
-                return true;
+            if (!sender.hasPermission("seeinv.command.use")) {
+                throw new IllegalStateException("No permission");
             }
-
-            if (args.length == 0) {
-                return false;
-            }
-
-            Player target = getServer().getPlayer(args[0]);
-            CompoundTag offlineData = null;
-
-            if (target == null) {
-                offlineData = getServer().getOfflinePlayerData(args[0], false);
-                if (offlineData == null) {
-                    sender.sendMessage("§cUnknown player: " + args[0]);
-                    return true;
-                }
-            }
-
-            if (sender.equals(target) && !sender.hasPermission("seeinv.self")) {
-                sender.sendMessage("§cYou don't have permission to use this command for own inventory");
-                return true;
-            }
-
-            ChestFakeInventory inv;
 
             if (args.length == 2 && args[1].equalsIgnoreCase("echest")) {
                 inv = new ChestFakeInventory();
                 if (offlineData != null) {
-                    setOfflineInventory(inv, offlineData, true);
+                    loadOfflineInventory(inv, offlineData, true);
                     inv.setName(offlineData.getString("NameTag") + "'s ender chest");
                 } else {
                     inv.setName(target.getName() + "'s ender chest");
@@ -54,7 +65,7 @@ public class Main extends PluginBase {
             } else {
                 inv = new DoubleChestFakeInventory();
                 if (offlineData != null) {
-                    setOfflineInventory(inv, offlineData, false);
+                    loadOfflineInventory(inv, offlineData, false);
                     inv.setName(offlineData.getString("NameTag") + "'s inventory");
                 } else {
                     inv.setName(target.getName() + "'s inventory");
@@ -62,83 +73,85 @@ public class Main extends PluginBase {
                 }
             }
 
-            inv.addListener(this::onSlotChange);
+            inv.addListener(this::onSlotView);
             ((Player) sender).addWindow(inv);
-        } else if (cmd.getName().equals("echest")) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage("§cYou can run this command only as a player");
-                return true;
+        } else if (cmd.getName().equals("editinv")) {
+            if (!sender.hasPermission("editinv.command.use")) {
+                throw new IllegalStateException("No permission");
             }
 
-            Player target;
-            CompoundTag offlineData = null;
-            ChestFakeInventory inv = new ChestFakeInventory();
-
-            if (args.length == 0) {
-                if (sender.hasPermission("seeinv.self")) {
-                    target = (Player) sender;
+            if (args.length == 2 && args[1].equalsIgnoreCase("echest")) {
+                inv = new ChestFakeInventory();
+                if (offlineData != null) {
+                    loadOfflineInventory(inv, offlineData, true);
+                    inv.setName(offlineData.getString("NameTag") + "'s ender chest");
                 } else {
-                    sender.sendMessage("§cYou don't have permission to use this command for own ender chest inventory");
-                    return true;
+                    target.removeWindow(target.getEnderChestInventory());
+
+                    inv.setName(target.getName() + "'s ender chest");
+                    inv.setContents(target.getEnderChestInventory().getContents());
+
+                    ecOpen.add(target.getName());
                 }
             } else {
-                target = getServer().getPlayer(args[0]);
+                inv = new DoubleChestFakeInventory();
+                if (offlineData != null) {
+                    loadOfflineInventory(inv, offlineData, false);
+                    inv.setName(offlineData.getString("NameTag") + "'s inventory");
+                } else {
+                    target.removeWindow(target.getInventory());
 
-                if (target == null) {
-                    offlineData = getServer().getOfflinePlayerData(args[0], false);
-                    if (offlineData == null) {
-                        sender.sendMessage("§cUnknown player: " + args[0]);
-                        return true;
-                    }
+                    inv.setName(target.getName() + "'s inventory");
+                    inv.setContents(target.getInventory().getContents());
+
+                    invOpen.add(target.getName());
                 }
             }
 
-            if (offlineData != null) {
-                setOfflineInventory(inv, offlineData, true);
-                inv.setName(offlineData.getString("NameTag") + "'s ender chest");
-            } else {
-                inv.setContents(target.getEnderChestInventory().getContents());
-                inv.setName(target.getName() + "'s ender chest");
-            }
-
-            inv.addListener(this::onSlotChange);
+            inv.addListener(this::onSlotEdit);
             ((Player) sender).addWindow(inv);
         }
 
         return true;
     }
 
-    private void onSlotChange(FakeSlotChangeEvent e) {
+    private void onSlotView(FakeSlotChangeEvent e) {
         if (e.getInventory() instanceof ChestFakeInventory) {
             boolean inv = e.getInventory() instanceof DoubleChestFakeInventory && e.getInventory().getName().contains("'s inventory");
             boolean ec = e.getInventory().getName().contains("'s ender chest");
             if (inv || ec) {
-                if (e.getPlayer().hasPermission("seeinv.takeitems")) {
-                    Player target = getServer().getPlayerExact(e.getInventory().getName().replace("'s ender chest", "").replace("'s inventory", ""));
-                    if (target == null) {
-                        e.getPlayer().sendMessage("§cOperation failed: target player is not online");
-                        e.setCancelled(true);
-                    } else if (inv && target.equals(e.getPlayer())) {
-                        e.getPlayer().sendMessage("§cOperation failed: can't edit own player inventory trough fake inventory");
-                        e.setCancelled(true);
-                    } else {
-                        getServer().getScheduler().scheduleTask(this, () -> {
-                            if (inv) {
-                                target.getInventory().setContents(e.getInventory().getContents());
-                            } else {
-                                target.getEnderChestInventory().setContents(e.getInventory().getContents());
-                            }
-                        });
-                    }
-                } else {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    private void onSlotEdit(FakeSlotChangeEvent e) {
+        if (e.getInventory() instanceof ChestFakeInventory) {
+            boolean inv = e.getInventory() instanceof DoubleChestFakeInventory && e.getInventory().getName().contains("'s inventory");
+            boolean ec = e.getInventory().getName().contains("'s ender chest");
+            if (inv || ec) {
+                Player target = getServer().getPlayerExact(e.getInventory().getName().replace("'s ender chest", "").replace("'s inventory", ""));
+                if (target == null) {
+                    e.getPlayer().sendMessage("§cOperation failed: target player is not online");
                     e.setCancelled(true);
+                } else if (inv && target.equals(e.getPlayer())) {
+                    e.getPlayer().sendMessage("§cOperation failed: can't edit own player inventory");
+                    e.setCancelled(true);
+                } else {
+                    getServer().getScheduler().scheduleTask(this, () -> {
+                        if (inv) {
+                            target.getInventory().setContents(e.getInventory().getContents());
+                        } else {
+                            target.getEnderChestInventory().setContents(e.getInventory().getContents());
+                        }
+                    });
                 }
             }
         }
     }
     
-    private static void setOfflineInventory(ChestFakeInventory inv, CompoundTag offlineData, boolean ec) {
-        if (ec) {
+    private static void loadOfflineInventory(ChestFakeInventory inv, CompoundTag offlineData, boolean echest) {
+        if (echest) {
             if (offlineData.contains("EnderItems") && offlineData.get("EnderItems") instanceof ListTag) {
                 ListTag<CompoundTag> inventoryList = offlineData.getList("EnderItems", CompoundTag.class);
                 for (CompoundTag item : inventoryList.getAll()) {
@@ -159,6 +172,39 @@ public class Main extends PluginBase {
                     } else {
                         inv.setItem(slot - 9, NBTIO.getItemHelper(item));
                     }
+                }
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onInteractEnderChest(PlayerInteractEvent e) {
+        if (e.getBlock() instanceof BlockEnderChest && ecOpen.contains(e.getPlayer().getName())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onOpenPlayerInventory(InventoryOpenEvent e) {
+        if (invOpen.contains(e.getPlayer().getName())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onFakeInventoryClose(InventoryCloseEvent e) {
+        if (e.getInventory() instanceof ChestFakeInventory) {
+            if (e.getInventory().getName().contains("'s inventory")) {
+                Player target = getServer().getPlayerExact(e.getInventory().getName().replace("'s inventory", ""));
+
+                if (target != null) {
+                    invOpen.remove(target.getName());
+                }
+            } else if (e.getInventory().getName().contains("'s ender chest")) {
+                Player target = getServer().getPlayerExact(e.getInventory().getName().replace("'s ender chest", ""));
+
+                if (target != null) {
+                    ecOpen.remove(target.getName());
                 }
             }
         }
